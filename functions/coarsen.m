dd7e048f508bbe41fb3c88e5f1ba9aa04676a6a7
Craig Maxwell <craigmxwell@gmail.com>
2020-06-22 13:46:58 +0200

    Update coarsen.m

--- functions/coarsen.m
+++ functions/coarsen.m
@@ -2,12 +2,19 @@
 % m_occ - occupancy map (percent of each cell occupied)
 % m_r   - raster map (binary, identifies if any occupancy in cell)
 
-% Notes
+%% Assumptions
 % - some error introduced by rounding - ignoring rows and column on sides
 % of matrix.
 
-% To do
+%% To do
 % - check distance estimations accurate
+% - return coarsen factor
+% - fix problem of scan map and environment map cell sizes not being an integer
+% factor of each other
+% - must be a better way of managing the environment map
+
+%% Change log
+% 22/06/2020 - improved readability
 
 %% Coarsen raster
 function [m_occ, m_r, l_c_x, l_c_y] = coarsen(m_p_in, ref, l_d)
@@ -22,8 +29,11 @@ function [m_occ, m_r, l_c_x, l_c_y] = coarsen(m_p_in, ref, l_d)
     % Coarsen to desired cell size - side to right is removed if necessary
     n_lat   = round(l_d/l_r_lat);
     n_lon   = round(l_d/l_r_lon);
-    l_c_x   = n_lat * m_per_deg_lat * ref.CellExtentInLatitude;  % length of new cells in x direction
-    l_c_y   = n_lon * m_per_deg_lon * ref.CellExtentInLongitude; % length of new cells in y direction
+    % length of new cells in x direction
+    l_c_x   = n_lat * m_per_deg_lat * ref.CellExtentInLatitude;  
+    % length of new cells in y direction
+    l_c_y   = n_lon * m_per_deg_lon * ref.CellExtentInLongitude;
+    
     g_d     = [floor(size(m_p_in,1)/n_lat), floor(size(m_p_in,2)/n_lon)];
     m_occ   = zeros(g_d(1), g_d(2));
     m_r     = zeros(g_d(1), g_d(2));
